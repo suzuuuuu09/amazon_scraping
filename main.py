4a784c202cb9c55ab034ec4cf748a862ca20498c
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2024-09-11 09:36:16 +0900

    データ取集終わったよ

--- main.py
+++ main.py
@@ -23,6 +23,6 @@ words = [
 
 scraper = ProductScraper()
 for word in words:
-    for i in range(1, 4):
-        scraper.scrape(f"https://sakura-checker.jp/itemsearch/?page={i}&sort=amazon&word={word}")
+    for i in range(3):
+        scraper.scrape(f"https://sakura-checker.jp/itemsearch/?page={i + 1}&sort=amazon&word={word}")
 scraper.quit()
