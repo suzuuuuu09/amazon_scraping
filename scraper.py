945f3c60391399e159d73439af4a05e586ff1d79
suzu <140299273+suzuuuuu09@users.noreply.github.com>
2024-09-27 09:23:23 +0900

    feat: 企業名のデータ収集追加

--- scraper.py
+++ scraper.py
@@ -35,18 +35,20 @@ class ProductScraper:
             product_name = self.driver.find_element(By.ID, "productTitle").text  # 商品名
             price = self.driver.find_element(By.CLASS_NAME, "a-price-whole").text  # 価格
             evaluations = self.driver.find_elements(By.CLASS_NAME, "a-size-base.a-color-base")  # 評価
-            evaluation = evaluations[1].text if len(evaluations) >= 2 else "評価情報が見つかりません"
+            evaluation = evaluations[1].text
             review = self.driver.find_element(By.ID, "acrCustomerReviewText").text.replace("個の評価", "")  # 評価数
             description = self.driver.find_element(By.ID, "feature-bullets").text.replace("› もっと見る", "").replace("この商品について", "")  # 商品説明
-            
+            company = self.driver.find_element(By.ID, "bylineInfo").text.replace("のストアを表示", "")
+
             current_url = self.driver.current_url  # 現在のURLを取得する
             product_id = current_url.replace("https://www.amazon.co.jp/gp/product/", "")[:10]  # 商品ID（？）
             self.driver.get(f"https://sakura-checker.jp/itemsearch/?word={product_id}")  # サクラチェッカーで商品のページを開く
             sakura_evaluation = self.driver.find_element(By.CLASS_NAME, "item-rating").text.replace("/5", "")  # サクラチェッカー内での評価
 
-            header = ["商品名", "価格", "評価", "評価数", "サクラ評価", "商品説明"]
-            data = [product_name, price, evaluation, review, sakura_evaluation, description]
+            header = ["商品名", "企業名", "価格", "評価", "評価数", "サクラ評価", "商品説明"]
+            data = [product_name, company, price, evaluation, review, sakura_evaluation, description]
             self.csv_writer.write_data(header, data)
+            print(company)
         
         except Exception as e:
             print(f"エラーが発生しました: {e}")
