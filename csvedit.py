f069132eb912726f9e94b448ed5e878b068037e9
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2024-10-01 17:21:39 +0900

    なおしたかも

--- csvedit.py
+++ csvedit.py
@@ -3,8 +3,8 @@ import pandas as pd
 df = pd.read_csv("data/product_info.csv")
 
 df['商品説明'] = df['商品説明'].str.strip()
-df['価格'] = df['価格'].replace('"', '').replace(',', '')
-df['評価数'] = df['評価数'].replace('"', '').replace(',', '')
+df['価格'] = df['価格'].str.replace('"', '').str.replace(',', '')
+df['評価数'] = df['評価数'].str.replace('"', '').str.replace(',', '')
 
 # print(df['商品説明'].isnull())
 df = df.dropna(subset=['商品説明'])
