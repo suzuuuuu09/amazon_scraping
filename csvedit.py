16be2f627d9f20087ca2104545eb770a4ad69d44
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2024-10-02 09:09:44 +0900

    update: 商品説明正確度

--- csvedit.py
+++ csvedit.py
@@ -9,4 +9,9 @@ df['評価数'] = df['評価数'].str.replace('"', '').str.replace(',', '')
 # print(df['商品説明'].isnull())
 df = df.dropna(subset=['商品説明'])
 
+with open("output.txt", "r") as f:
+    seikakudo_data = f.read().split("\n")
+
+df['商品説明正確度'] = seikakudo_data
+
 df.to_csv("data/processed_product_info.csv", index=False)
